758d433014c8189f7cb963dc04dc236392458079
Alex Williams <alexjwilliams1@gmail.com>
2020-08-04 13:14:11 -0700

    initializing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <img src="https://pngimg.com/uploads/github/github_PNG15.png" alt="GitHub Logo" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
